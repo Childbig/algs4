7a578ba59833a182fcdad2944a767c6c54f96575
zhangxuan <626951363@qq.com>
2019-03-15 22:38:34 +0800

    第1节学习内容

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -29,28 +29,27 @@ public class MergeSort {
 
 
     public static void sort(Comparable[] a, int lo, int hi) {
-        if (lo >= hi) return;
-        int mid = lo + (hi - lo)/2;
+        if (lo > hi) return;
 
+        int mid = hi - lo / 2;
         sort(a, lo, mid);
         sort(a, mid + 1, hi);
         merge(a, lo, mid, hi);
     }
 
     private static void merge(Comparable[] a, int lo, int mid, int hi) {
-        int i = lo, j = mid + 1;
+       int i = lo;
+       int j = mid + 1;
 
+       for (int k = 0; k <= hi; k++)
+           aux[k] = a[k];
 
-        for(int k = lo; k <= hi; k ++) {
-            aux[k] = a[k];
-        }
-
-        for(int k = lo; k <= hi; k++) {
-            if      (i > mid)        a[k] = aux[j++];
-            else if (j > hi)         a[k] = aux[i++];
-            else if (less(aux[j], aux[i])) a[k] = aux[j++];
-            else                                 a[k] = aux[i++];
-        }
+       for (int k = lo; k <= hi; k++) {
+           if       (i > mid)           aux[k] = a[j++];
+           else if  (j > hi)            aux[k] = a[i++];
+           else if (less(a[j], a[i]))   aux[k] = a[j++];
+           else                         aux[k] = a[i++];
+       }
     }
     private static Boolean less(Comparable a, Comparable b) {
         return a.compareTo(b) < 0;
@@ -66,5 +65,19 @@ public class MergeSort {
         Integer[] a = {1, 2, 3, 4, 7, 1, 3, 2};
         sort(a);
         System.out.println(a.toString());
+    }
+
+    /**
+     * 自底向上的归并
+     * @param a
+     */
+    public static void iterSort(Comparable[] a) {
+        int N = a.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz + sz) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz -1, N - 1));
+            }
+        }
     }
 }
--- src/sort/Shell.java
+++ src/sort/Shell.java
@@ -17,11 +17,12 @@ public class Shell {
         while (h < N/3) h = 3 * h + 1;
 
         while (h >= 1) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j >= h && less(a[j], a[j - h]); j -=h) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -=h) {
                     exch(a, j, j - h);
                 }
             }
+            h = h/3;
         }
 
     }
@@ -32,8 +33,14 @@ public class Shell {
 
     private static void exch(Comparable[] a, int i, int j) {
         Comparable swap = a[j];
-        a[i] = a[j];
-        a[j] = swap;
+        a[j] = a[i];
+        a[i] = swap;
+    }
+
+
+    public static void main(String[] args) {
+        String[] a = {"S", "H","E", "L","L","S","O","R","T","E"};
+        sort(a);
     }
 
 }
